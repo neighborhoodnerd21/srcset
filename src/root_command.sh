a9b98d1328062c5b6dcd8bea8078f4df53172fd1
NeighborhoodNerd21 <neighborhoodnerd21@gmail.com>
2026-04-18 08:53:31 -0400

    initial state

--- src/root_command.sh
+++ src/root_command.sh
@@ -1,5 +1,15 @@
+large=${args[--large]}
+
 if [ -d "${args[--input]}" ]; then
-    manyfiles
+  if [[ $large ]]; then
+    large_multi
+  else
+    small_multi
+  fi
 else
-    singlefile
+  if [[ $large ]]; then
+    large_single
+  else
+    small_single
+  fi
 fi
