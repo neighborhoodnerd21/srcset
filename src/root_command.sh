8404bd2e41ca7e26e40b3a8458d326365ec5bdfe
NeighborhoodNerd21 <neighborhoodnerd21@gmail.com>
2026-04-20 13:05:29 -0400

    changed a lot

--- src/root_command.sh
+++ src/root_command.sh
@@ -1,22 +1,46 @@
 large=${args[--large]}
 pngfiles=${args[--png]}
+declare -a files
+
+function get_files() {
+  readarray -d '' files < <(find "${IMG_PTH}" -maxdepth 1 -iname "${IMG_TYPE}" -type f -print0)
+  # add check for empty array
+  if [ ${#files[@]} -eq 0 ]; then
+    echo "No files found matching pattern ${IMG_TYPE} in directory ${IMG_PTH}"
+    exit 1
+  fi
+}
+
+function mk_srcset() {
+  for i in "${files[@]}"; do
+    name=$(basename "${i}")
+    mkdir "${name%.*}" || {
+      echo "Failed to create directory for ${name}"
+      exit 1
+    }
+    for j in "${SIZES[@]}"; do
+      magick "${name}" -resize "${j}x" "${name%.*}/${j}w.${name##*.}" || {
+        echo "Failed to resize ${name} to ${j}w"
+        exit 1
+      }
+      unset "$name"
+    done
+  done
+}
 
 if [[ $pngfiles ]]; then
   IMG_TYPE="*.png"
-else
-  IMG_TYPE="*.jpg"
 fi
 
-if [ -d "${args[--input]}" ]; then
-  if [[ $large ]]; then
-    large_multi
-  else
-    small_multi
-  fi
-else
-  if [[ $large ]]; then
-    large_single
-  else
-    small_single
-  fi
+if [[ $large ]]; then
+  SIZES=("${LG_SIZES[@]}")
 fi
+
+if [ -d "${args[input]}" ]; then
+  get_files
+  mk_srcset
+else
+  mk_srcset
+fi
+
+exit 0
